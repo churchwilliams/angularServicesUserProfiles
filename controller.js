1de930a153e9a51bdfaa5a96d5f163adacb5dae8
Eric Williams <eric@churchwilliams.com>
2015-08-31 09:42:16 -0600

    simplified the controller logic

--- controller.js
+++ controller.js
@@ -3,10 +3,5 @@ var app = angular.module('userProfiles');
 app.controller('mainController', function($scope, mainService){
 	$scope.message = "angular is working meng!";
 
-	$scope.getUsers = function() {
-        $scope.users = mainService.getData();
-    };
-
-    $scope.getUsers();
-    console.log($scope.users);
+ 	$scope.users = mainService.getData();
 });
